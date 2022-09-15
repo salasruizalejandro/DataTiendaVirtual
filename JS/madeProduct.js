fc49425298b15325be3c8fb32fba627536e57187
Alejandro Ruiz <salasruizalejandro1227@gmail.com>
2022-09-15 12:04:25 -0500

    Creacion de los productos

---
+++ JS/madeProduct.js
@@ -0,0 +1,15 @@
+let url = document.querySelector('#urlImagen');
+let categoria = document.querySelector('#categoria');
+let nombreProducto = document.querySelector('#Nombredeproducto');
+let precio = document.querySelector('#precio');
+let textDescripcion = document.querySelector('#descripcionProducto')
+
+
+
+function recogerDatos(){
+    console.log(url)
+    console.log(categoria)
+    console.log(nombreProducto)
+    console.log(precio)
+    console.log(textDescripcion)
+}
