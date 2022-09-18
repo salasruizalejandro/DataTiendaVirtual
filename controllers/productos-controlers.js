dc36dbebc966b4ab01b443b7a412e87c83dae5aa
Alejandro Ruiz <salasruizalejandro1227@gmail.com>
2022-09-18 18:07:48 -0500

    css class="card", creacion de producto

--- controllers/productos-controlers.js
+++ controllers/productos-controlers.js
@@ -1,1 +1,18 @@
 import {listaProductos} from "../servicios/productos-servicios.js"
+
+const nuevoProducto =(name,price,imagenUrl)=>{
+    const nuevaCard = document.createElement("div")
+    const contenido = `
+    <ul>
+        <li>
+            <img class="ImgProducSc" src="${imagenUrl}" alt="Productos">
+            <h4 class="producto">${name}</h4>
+            <h5 class="valor">${price}</h5>
+            <a class="verProducto" href="">ver producto</a>
+        </li>
+    </ul>
+    `;
+    nuevaCard.innerHTML=contenido;
+    nuevaCard.classList.add("nuevaCard");
+    return nuevaCard;
+}
