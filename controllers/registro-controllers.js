b4ee5f267e1e2696fe7b83e089cb2c4a364bb5ce
Alejandro Ruiz <salasruizalejandro1227@gmail.com>
2022-09-21 08:13:33 -0500

    ImagenUrl, solo  funciona con png

--- controllers/registro-controllers.js
+++ controllers/registro-controllers.js
@@ -11,7 +11,10 @@ formulario.addEventListener("submit", (e) => {
     const nombreProducto = document.querySelector("[data-nombreProducto]").value;
     const precio = document.querySelector("[data-precio]").value;
     const comentarios = document.querySelector("[data-comentario]").value;
+
+
     clientServices.crearCliente(url, categoria, nombreProducto, precio, comentarios).then( respuesta => {
+        window.location.href = "/addProductos.html"
         console.log(respuesta)
     }).catch(err => console.log(err))
 });
